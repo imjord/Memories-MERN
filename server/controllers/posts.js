ee47fb9e0a04adfa7199ec6947b44c189444c73b
imjord <dean.jay.goat@gmail.com>
2022-03-14 18:56:10 -0600

    updated models and controllers

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,24 @@
-export const getPosts = (req, res) => {
-  res.send("This works");
+import Posts from "../models/Posts.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    // async cause it takes time to find posts in database
+    const posts = await Posts.find();
+    console.log(posts);
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPosts = async (req, res) => {
+  const post = req.body;
+  const newPost = new Posts(post);
+
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
--- server/models/Posts.js
+++ server/models/Posts.js
@@ -1,7 +1,21 @@
-import { Schema, model } from "mongoose";
+import mongoose from "mongoose";
 
-const PostSchema = new Schema({
+const PostSchema = mongoose.Schema({
   title: String,
   message: String,
   creator: String,
+  tags: [String],
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
 });
+
+const Posts = mongoose.model("Posts", PostSchema);
+
+export default Posts;
